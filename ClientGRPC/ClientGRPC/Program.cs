787a7631f4cb6e55b84b821ba4bd4fdaf0ed5ff5
JiroFg <mechcromancer@gmail.com>
2023-05-12 16:20:52 -0600

    first commit

---
+++ ClientGRPC/ClientGRPC/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Net.Client;
+using ClientGRPC;
+
+using (var channel = GrpcChannel.ForAddress("https://localhost:7116"))
+{
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Alex"
+    });
+
+    Console.WriteLine(reply.Message);
+    Console.ReadKey();
+}
